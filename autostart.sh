50dc38fbce630900a978a4a1ddbb2b9357b3faf0
Farzeen <happycoder97@protonmail.com>
2020-11-02 19:05:15 +0530

    basic autostart

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 date >>$HOME/dwm_autostart
+
+# launch only if the exe is not running
+# prevents duplicate launches when restarting
+function single_instance() {
+    exe=$1
+    shift 1
+    args=$*
+    pgrep -laf $(which $exe) || pgrep -laf $exe || $exe $args &
+}
+
+single_instance firefox
+single_instance notion-app
+single_instance dwm-status $HOME/.config/dwm-status.toml
+single_instance sxhkd
